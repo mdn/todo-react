3f968c5e8672610e5ddfaa995dcef0e3691708cd
Chris Mills <cmills@mozilla.com>
2020-05-15 12:25:06 +0100

    initial commit of the content

---
+++ build/precache-manifest.81f54de5433e54cc32f118f7ba0769ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0e0cb601f2f1d6f964305788f02bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cafd1638bbf6d408ec28",
+    "url": "/static/css/main.855a11a9.chunk.css"
+  },
+  {
+    "revision": "1d8b48033a1ea42196b4",
+    "url": "/static/js/2.c048c89e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c048c89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafd1638bbf6d408ec28",
+    "url": "/static/js/main.48c2f919.chunk.js"
+  },
+  {
+    "revision": "e3297ad90e81d786f5fd",
+    "url": "/static/js/runtime-main.57c5baf0.js"
+  }
+]);
