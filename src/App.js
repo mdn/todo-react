8f7b7bc49ca990ecbb834006ea76b510dbc4f18c
Chris Mills <cmills@mozilla.com>
2020-06-23 08:12:30 +0100

    adding role="list" back in

--- src/App.js
+++ src/App.js
@@ -108,6 +108,7 @@ function App(props) {
         {headingText}
       </h2>
       <ul
+        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
