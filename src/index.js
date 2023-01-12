01b1a2ecdb1967f0cabcd27eedc690ade8ddb159
Brian Thomas Smith <brian@smith.berlin>
2023-01-12 14:59:35 +0100

    chore(deps): Bump core deps, remove ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-2", name: "Repeat", completed: false },
 ];
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App tasks={DATA} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
