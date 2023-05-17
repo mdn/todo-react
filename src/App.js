517f7098cb5ef533f6508b8170fe9024f6181859
Géry Ogam <gery.ogam@gmail.com>
2023-05-17 02:57:52 +0200

    Add a page title

--- src/App.js
+++ src/App.js
@@ -96,6 +96,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
+      <h1>Todo</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
