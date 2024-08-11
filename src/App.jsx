a336b5a7ae73664333d21096c90218365a209f16
caongocthien <61340843+caongocthien@users.noreply.github.com>
2024-08-11 23:10:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>TodoMatic thien</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
