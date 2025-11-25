ae12005eb2ceb304d6c6a244c26bac7b3b5c3ac2
quan-ngo-ts <quan.ngo@trustingsocial.com>
2025-11-25 10:51:57 +0700

    feat: update form title

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,7 +20,7 @@ function Form(props) {
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
+          What needs to do?
         </label>
       </h2>
 
